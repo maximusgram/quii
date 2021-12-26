2b96e882989d4a875e22871da597b473d1baca9a
Maximus Graham <maxigrampan@gmail.com>
2021-12-26 15:46:50 +0530

    refactor: independent tests

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 )
@@ -30,21 +31,38 @@ func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 }
 
 func TestCountdown(t *testing.T) {
-	spySleepPrinter := &SpyCountdownOperations{}
-	Countdown(spySleepPrinter, spySleepPrinter)
+    t.Run("prints 3 to Go!", func(t *testing.T) {
+        buffer := &bytes.Buffer{}
+        Countdown(buffer, &SpyCountdownOperations{})
 
-	want := []string{
-		sleep,
-		write,
-		sleep,
-		write,
-		sleep,
-		write,
-		sleep,
-		write,
-	}
+        got := buffer.String()
+        want := `3
+2
+1
+Go!`
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    })
 
-	if !reflect.DeepEqual(spySleepPrinter.Calls, want) {
-		t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
-	}
+    t.Run("sleep before every print", func(t *testing.T) {
+
+        spySleepPrinter := &SpyCountdownOperations{}
+        Countdown(spySleepPrinter, spySleepPrinter)
+    
+        want := []string{
+            sleep,
+            write,
+            sleep,
+            write,
+            sleep,
+            write,
+            sleep,
+            write,
+        }
+    
+        if !reflect.DeepEqual(spySleepPrinter.Calls, want) {
+            t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+        }
+    })
 }
